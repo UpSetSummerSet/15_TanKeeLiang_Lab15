80ecdcfb0f3c4420f53238afb706f31b849de48f
UpSetSummerSet <72717739+UpSetSummerSet@users.noreply.github.com>
2021-01-13 16:00:26 +0800

    Done

---
+++ 15_TanKeeLiang_Lab15/Assets/Scripts/BGM.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGM : MonoBehaviour
+{
+    float volspeed = 0.2f;
+
+    private AudioSource audioSource;
+
+    public AudioClip[] audioclipArr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        int rand = Random.Range(0, audioclipArr.Length);
+        audioSource.PlayOneShot(audioclipArr[rand]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            audioSource.Stop();
+            int rand = Random.Range(0, audioclipArr.Length);
+            audioSource.PlayOneShot(audioclipArr[rand]);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            audioSource.volume += Time.deltaTime * volspeed;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            audioSource.volume -= Time.deltaTime * volspeed;
+        }
+    }
+}
---
+++ 15_TanKeeLiang_Lab15/Assets/Scripts/PlayerController15.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController15 : MonoBehaviour
+{
+    float jumpForce = 8.0f;
+    int IsGround = 0;
+    int JumpCounter;
+
+    private Rigidbody playerRB;
+    private AudioSource audioSource;
+
+    public GameObject JumpText;
+    public AudioClip[] AudioClipArr;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRB = GetComponent<Rigidbody>();
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && IsGround < 1)
+        {
+            playerRB.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+            JumpCounter++;
+            JumpText.GetComponent<Text>().text = "Jump: " + JumpCounter;
+
+            IsGround++;
+
+            int rand = Random.Range(0, AudioClipArr.Length);
+            audioSource.PlayOneShot(AudioClipArr[rand]);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            IsGround = 0;
+        }
+    }
+}
